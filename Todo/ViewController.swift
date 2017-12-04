7b4c3e5519db0c9367eebbce7a9e08c6ca094c18
Daniel Chen <daniel.chen@creditkarma.com>
2017-12-03 16:53:39 -0800

    test

---
+++ Todo/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Daniel Chen on 11/10/17.
+//  Copyright © 2017 Daniel Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+}
+
