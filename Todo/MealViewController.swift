5f05208c4e6d64c7906b21690ccbdf5cfcdb0de2
Daniel Chen <daniel.chen@creditkarma.com>
2017-12-06 21:00:08 -0800

    finish tutorial

--- Todo/MealViewController.swift
+++ Todo/MealViewController.swift
@@ -24,6 +24,14 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         nameTextField.delegate = self
+        
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         updateSaveButtonState()
     }
 
@@ -61,7 +69,14 @@ class MealViewController: UIViewController, UITextFieldDelegate,UIImagePickerCon
     //MARK: Navigation
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
