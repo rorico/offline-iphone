a35a30aac103a6c51346753125b9a53c59e4fab4
Daniel Chen <daniel77chen@hotmail.com>
2017-12-07 19:44:37 -0800

    empty ios app

---
+++ offline-iphone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  offline-iphone
+//
+//  Created by Daniel Chen on 12/6/17.
+//  Copyright © 2017 Daniel Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
